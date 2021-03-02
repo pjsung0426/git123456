2f0233ced1c23854fd94d5205cfd9ac699772cc5
js.park <js0426@naver.com>
2021-03-02 17:55:30 +0900

    기본 프로젝트 생성

---
+++ src/main/mainClass.java
@@ -0,0 +1,23 @@
+package main;
+import java.util.*;
+public class mainClass {
+
+	public static void main(String[] args) {
+		System.out.println("Git Test");
+		// kim - 로그인 관련 기능 만드세요
+		// cho - 회원 관리 기능을 만드세요
+		
+		Scanner scanner = new Scanner(System.in);
+		int num;
+		while(true) {
+			System.out.println("1. 로그인 2. 회원관리");
+			num = scanner.nextInt();
+			switch(num) {
+				case 1:
+					break;
+				case 2:
+					break;
+			}
+		}
+	}
+}
